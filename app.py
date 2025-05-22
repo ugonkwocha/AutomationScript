d1ef34df4273f302cb0f83696d63aee5b934e7a3
ugonkwocha <ugochukwu.nkwocha@gmail.com>
2025-05-22 11:02:01 -0400

    Pipeline Update - Debug  CICD pipeline for application

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<h1>Welcome to Coding</h1>'
  
 # main driver function
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
